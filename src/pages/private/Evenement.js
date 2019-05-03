2787f84c786d2b13c945f26e60490b815f6208b0
Irina Despot <irinadespot@gmail.com>
2019-05-03 08:07:26 +0200

    Evenement details

--- src/pages/private/Evenement.js
+++ src/pages/private/Evenement.js
@@ -1,13 +1,76 @@
 import React from 'react';
 import PrivateLayout from "../../components/PrivateLayout";
+import $ from "jquery";
+import Config from "../../Config";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import moment from "moment";
 
 export default class Evenement extends React.Component {
 
+    state = {
+        event: null
+    };
+
+    componentDidMount() {
+        $.ajax({
+            url: Config.apiUrl + "/calendar/" + this.props.match.params.event,
+            method: "GET",
+            success: res => {
+                this.setState({event: res});
+            }
+        });
+    }
+
     render() {
+        let event = this.state.event;
+        if (!event) return <h1>Loading...</h1>;
         return (
             <PrivateLayout>
-                <h1>Evenement</h1>
+                <div className="container py-4">
+                    <div className="row">
+                        <div className="col-12 col-md-6">
+                            {this.renderDetails(event)}
+                        </div>
+                        <div className="col-12 col-md-6">
+                            <h1>Discussions</h1>
+                        </div>
+                        <div className="col-12">
+                            <h1>Participants</h1>
+                        </div>
+                    </div>
+                </div>
             </PrivateLayout>
         );
+    }
+
+    renderDetails(event) {
+        return (
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{event.title}</h5>
+                    <p className="card-text">{event.description}</p>
+                </div>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">{this.renderDate(event)}</li>
+                    <li className="list-group-item">
+                        <FontAwesomeIcon icon="eye" className="mr-2"/>
+                        {Config.privacy[event.privacy]}
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+
+    renderDate(event) {
+        let date = [
+            <FontAwesomeIcon icon={["far", "calendar-alt"]} className="mr-2"/>,
+            <span>{moment(event.start).format("DD.MM.YYYY")}</span>
+        ];
+
+        if (event.end && event.end !== event.start) {
+            date.push(<FontAwesomeIcon icon="arrow-right" className="mx-2"/>);
+            date.push(<span>{moment(event.end).format("DD.MM.YYYY")}</span>);
+        }
+        return date;
     }
 }
