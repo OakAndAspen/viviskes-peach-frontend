7af0fc95172e5172f8cb0b407bf987b83a5b6c9e
Irina Despot <irinadespot@gmail.com>
2019-11-08 21:22:37 +0100

    Fixed image upload in CKEditor

--- src/components/CustomEditor.js
+++ src/components/CustomEditor.js
@@ -1,29 +1,29 @@
 import InlineEditor from "@ckeditor/ckeditor5-build-inline";
 import CKEditor from "@ckeditor/ckeditor5-react";
+import {apiUrl} from "config";
 import PropTypes from "prop-types";
 import React from "react";
 
 export default class CustomEditor extends React.Component {
     render() {
         return (
-        <CKEditor
-            editor={InlineEditor}
-            data={this.props.content}
-            config={{
-                //plugins: [/*SimpleUploadAdapter*/],
-                placeholder: this.props.placeholder
-                /*simpleUpload: {
-                    uploadUrl: apiUrl + "/file",
-                    headers: {
-                        //'X-CSRF-TOKEN': 'CSFR-Token',
-                        Authorization: 'Bearer ' + localStorage.authKey
-                    }
-                }*/
-            }}
-            onChange={(event, editor) => {
-                this.props.onChange(editor.getData());
-            }}
-        />
+            <CKEditor
+                editor={InlineEditor}
+                data={this.props.content}
+                config={
+                    {
+                        ckfinder: {
+                            uploadUrl: apiUrl + '/file',
+                            headers: {
+                                Authorization: 'Bearer ' + localStorage.authKey
+                            }
+                        },
+                        placeholder: this.props.placeholder
+                    }}
+                onChange={(event, editor) => {
+                    this.props.onChange(editor.getData());
+                }}
+            />
         );
     }
 }
